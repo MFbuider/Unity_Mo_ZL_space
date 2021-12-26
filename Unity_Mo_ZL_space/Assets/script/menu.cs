a1da1ed44b7acd635b548563e6023913d86e7d8e
MFbuider <62496746+MFbuider@users.noreply.github.com>
2021-12-26 12:44:50 +0800

    C#

--- Unity_Mo_ZL_space/Assets/script/menu.cs
+++ Unity_Mo_ZL_space/Assets/script/menu.cs
@@ -5,7 +5,38 @@ using UnityEngine.UI;
 
 public class menw : MonoBehaviour
 {
-    // Start is called before the first frame update
+   /* public class Menu : MonoBehaviour
+    {
+        [Header("擺放BGM的Prefab物件")]
+        public GameObject BGM;
+
+        [Header("控制聲音開關")]
+        public bool ControlSound;
+        [Header("開聲音的圖片")]
+        public Sprite OpenSoundTex;
+        [Header("關聲音的圖片")]
+        public Sprite CloseSoundTex;
+        [Header("放入聲音的按鈕")]
+        public Image SoundButton;*/
+
+        [Header("控制解析度的下拉選單")]
+        public Dropdown ScreenSizeDropdown;
+        // Start is called before the first frame update
+        void Start()
+        {
+            //BGM => 背景音樂
+            //GameObject.Find("物件名稱")
+            //GameObject.FindWithTag("物件標籤")
+            //找尋場景上是否有BGM物件，如果找尋BGM數量為0，代表場上沒有任何BGM物件
+           /* if (GameObject.FindGameObjectsWithTag("BGM").Length <= 0)
+            {
+                //Instantiate動態生成(要生成的物件)
+                Instantiate(BGM);
+            }*/
+
+        }
+        // Start is called before the first frame update
+        public dropdown ScreenSizeDropdown;
     void Start()
     {
         
@@ -21,4 +52,36 @@ public class menw : MonoBehaviour
 
     }
 
+    
+    /* public void SoundControl()
+    {
+
+        ControlSound = !ControlSound;
+        if (ControlSound)
+        {
+            AudioListener.pause = true;
+            SoundButton.sprite = Resources.Load<Sprite>("pause-icon");
+
+        }
+        else
+        {
+            AudioListener.pause = false;
+            SoundButton.sprite = Resources.Load<Sprite>("voice Button");
+        }
+
+    } 
+    */
+
+
+    public void ChangeScreenSize()
+    {
+        if (ScreenSizeDropdown.value == 0)
+        {
+            Screen.SetResolution(1080, 1920, false);
+        }
+        else
+        {
+            Screen.SetResolution(480, 800, false);
+        }
+    }
 }
